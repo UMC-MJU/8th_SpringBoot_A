b38512b57b8dbd746cf08774c4bf4daa8d3df724
L3416yst <L3416yst@gmail.com>
2025-05-12 15:03:21 +0900

    6주차_수정

--- 예슬_이윤서/spring/src/main/java/umc/study/repository/quest/QuestRepository.java
+++
@@ -1,7 +0,0 @@
-package umc.study.repository.quest;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import umc.study.domain.Quest;
-
-public interface QuestRepository extends JpaRepository<Quest, Long>, QuestRepositoryCustom {
-}
