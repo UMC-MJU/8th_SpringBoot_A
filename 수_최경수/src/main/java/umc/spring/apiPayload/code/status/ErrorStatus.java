28a450467725f5cf67b3ae4e1227ef742a6572e3
choikyungsoo <ryan1954@naver.com>
2025-05-23 17:03:56 +0900

    FEAT: api 완성 캬캬컄컄

--- 수_최경수/src/main/java/umc/spring/apiPayload/code/status/ErrorStatus.java
+++ 수_최경수/src/main/java/umc/spring/apiPayload/code/status/ErrorStatus.java
@@ -25,6 +25,9 @@ public enum ErrorStatus implements BaseErrorCode {
   // 음식 카테고리 관련 에러
   FOOD_CATEGORY_NOT_FOUND(HttpStatus.NOT_FOUND, "CATEGORY4001", "해당하는 음식 카테고리를 찾을 수 없습니다."),
 
+  // 가게 관련 에러
+  STORE_NOT_FOUND(HttpStatus.NOT_FOUND, "STORE4001", "해당하는 가게가 없습니다."),
+
   //테스트용
   TEMP_EXCEPTION(HttpStatus.BAD_REQUEST, "TEMP4001", "테스트용");
 
