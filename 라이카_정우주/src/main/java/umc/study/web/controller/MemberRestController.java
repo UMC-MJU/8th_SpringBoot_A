4814c2b7f34b4106f3e87b380f625d615ae176c6
woojoo48 <wjddnwn0408@gmail.com>
2025-05-23 18:36:10 +0900

    git error 해결

---
+++ 라이카_정우주/src/main/java/umc/study/web/controller/MemberRestController.java
@@ -0,0 +1,22 @@
+package umc.study.web.controller;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import umc.study.apiPayload.ApiResponse;
+import umc.study.web.dto.MemberResponseDTO;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/members")
+public class MemberRestController {
+
+    private final MemberCommandService memberCommandService;
+
+    @PostMapping("/")
+    public ApiResponse<MemberResponseDTO.JoinResultDTO> join(@RequestBody @Valid MemberRequestDTO.JoinDto request){
+        return null;
+    }
+}
