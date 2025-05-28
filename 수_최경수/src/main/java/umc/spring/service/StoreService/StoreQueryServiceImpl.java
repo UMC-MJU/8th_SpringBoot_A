b183980aa12b24048d6ca1b7e4da26513c3970e7
choikyungsoo <ryan1954@naver.com>
2025-05-28 18:50:09 +0900

    FEAT: 9주차 워크북 미션 완료

--- 수_최경수/src/main/java/umc/spring/service/StoreService/StoreQueryServiceImpl.java
+++ 수_최경수/src/main/java/umc/spring/service/StoreService/StoreQueryServiceImpl.java
@@ -1,8 +1,12 @@
 package umc.spring.service.StoreService;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
+import umc.spring.domain.Review;
 import umc.spring.domain.Store;
+import umc.spring.repository.ReviewRepository.ReviewRepository;
 import umc.spring.repository.StoreRepository.StoreRepository;
 
 import java.util.List;
@@ -13,6 +17,7 @@ import java.util.Optional;
 public class StoreQueryServiceImpl implements StoreQueryService{
 
   private final StoreRepository storeRepository;
+  private final ReviewRepository reviewRepository;
 
   @Override
   public Optional<Store> findStore(Long id) {
@@ -24,5 +29,12 @@ public class StoreQueryServiceImpl implements StoreQueryService{
     List<Store> filteredStores = storeRepository.dynamicQueryWithBooleanBuilder(name, score);
     filteredStores.forEach(store -> System.out.println("Store: " + store));
     return filteredStores;
+  }
+
+  @Override
+  public Page<Review> getReviewList(Long StoreId, Integer page) {
+    Store store = storeRepository.findById(StoreId).get();
+    Page<Review> StorePage = reviewRepository.findAllByStore(store, PageRequest.of(page, 5));
+    return StorePage;
   }
 }
